6cec6e6829ee257b9b00630527ee4e3a4cb4adda
zacscodingclub <zbaston@gmail.com>
2017-03-23 20:04:16 -0400

    move require statements to class file

--- lib/cola_ttb.rb
+++ lib/cola_ttb.rb
@@ -1,7 +1,3 @@
-require "open-uri"
-require "mechanize"
-require "nokogiri"
-
 require "cola_ttb/version"
 require "cola_ttb/scraper"
 require "cola_ttb/csv"
--- lib/cola_ttb/scraper.rb
+++ lib/cola_ttb/scraper.rb
@@ -1,11 +1,25 @@
+require "open-uri"
+require "mechanize"
+require "nokogiri"
+
 module ColaTtb
   class Scraper
     SEARCH_URL         = "https://www.ttbonline.gov/colasonline/publicSearchColasBasic.do"
     BEVERAGE_PRINT_URL = "https://www.ttbonline.gov/colasonline/viewColaDetails.do?action=publicFormDisplay&ttbid="
 
     def self.call
-
+      search_criteria = {
+        :dateCompletedFrom     => 03/20/2017,
+        :dateCompletedTo       => 03/23/2017,
+        :productOrFancifulName => "%",
+        :productNameSearchType => "E",
+        :classTypeFrom         => 901,
+        :classTypeTo           => 956,
+        :originCode            => "%",    => "%",
+      }
     end
+
+
     def self.mechanize_setup
       mech = Mechanize.new
       mech.user_agent = 'Windows Mozilla'
